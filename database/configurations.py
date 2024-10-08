089ad6789a84a870d45d4ceb84a96e41ac6ffe3d
Chris-Emma <chrisemmaofficial@gmail.com>
2024-10-08 13:27:20 +0000

    config

--- database/configurations.py
+++ database/configurations.py
@@ -2,4 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#creating sqlite engine
+engine = create_engine("sqlite:///database/todo.db")
+
+#creating declarative_base instance
+Base = declarative_base()
+
+#creating session local for session maker
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
+def init_db():
+    Base.metadata.create_all(engine)
 
