580cc061f26ca5235c457859012897f491fb9dc8
Chris-Emma <chrisemmaofficial@gmail.com>
2024-10-21 15:49:01 +0000

    api routes

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from fastapi import FastAPI, Response
+import json
+from fastapi import FastAPI, Depends, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from database.configurations import init_db, db_session
+from database.models import Item
+from database.schema import ItemSchema
 
 #configure app
 app = FastAPI()
 
-#API
-@app.get("/")
-def awesome_api():
-    return Response("Welcome to my api")
+#configuring static route
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#configuring templates
+templates = Jinja2Templates(directory="templates")
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/api/todo")
+def getItems(session: Session =Depends(db_session)):
+    item = session.query(Item).all()
+    return Item
