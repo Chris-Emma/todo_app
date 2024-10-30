49084ce4d75adde6c60bfe918c9dbf20dc12f3b4
Chris-Emma <chrisemmaofficial@gmail.com>
2024-10-30 10:35:26 +0000

    debbuging configuartions.py

--- database/configurations.py
+++ database/configurations.py
@@ -2,17 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#creating sqlite engine
+# Create sqlite engine instance
 engine = create_engine("sqlite:///database/todo.db")
 
-#creating declarative_base instance
+# Create declaritive base meta instance
 Base = declarative_base()
 
-#creating session local for session maker
+# Create session local class for session maker
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
 
 def init_db():
     Base.metadata.create_all(engine)
+
 
 def db_session():
     session = SessionLocal()
